411ca5a6266bcbf8b5a54f750967285ce50cad75
leeejin <asd72621@naver.com>
2024-02-11 23:06:55 +0900

    마이페이지 구현중

--- src/components/user/mypage.js
+++ src/components/user/mypage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 import store from '../../util/redux_storage';
+
 export default function MyPage() {
     const navigate = useNavigate();
     const [userId, setUserId] = useState(store.getState().userId); //리덕스에 있는 userId를 가져옴
@@ -9,38 +10,32 @@ export default function MyPage() {
 
     const [subBoxVisible, setSubBoxVisible] = useState({ reserveList: false, payList: false });
 
-    const handleBox = (boxNumber) => {
-        setSubBoxVisible({
-            ...subBoxVisible,
-            [boxNumber]: !subBoxVisible[boxNumber]
-        });
+
+
+    const handleLocation = (selectedBox) => {
+        if (selectedBox === 'reserveList') navigate('/ReservedList');
+        else if (selectedBox === 'payList') navigate('/PaidList');
     }
+
     const BoxListContainer = ({ onClick, listname, selectedBox }) => {
         return (
-            <div className="box-container">
-                <div className="btn" onClick={onClick}>
-                    <h3>{listname}</h3>
-                    <span>-</span>
-                </div>
-                {
-                    subBoxVisible[selectedBox] && <div>
-                        까궁
-                        <span className="btn" onClick={()=>{}}>더보기</span>
-                    </div>
-                }
-            </div>
+            <li className="btn" onClick={onClick}>
+                <h3>{listname}</h3>
+            </li>
+
         )
     }
+
     return (
         <div className="background">
             <div className="backBox">
                 <div className="innerBox">
                     <h3>{nickname}님의 마이페이지</h3>
-
-                    <BoxListContainer listname="예약목록" selectedBox='box1' onClick={() => handleBox('box1')} />
-                    <BoxListContainer listname="결제목록" selectedBox='box2' onClick={() => handleBox('box2')} />
-                   
-
+                    <ul className="box-container">
+                        <BoxListContainer listname="예약목록" selectedBox='reserveList' onClick={() => handleLocation('reserveList')} />
+                        <hr />
+                        <BoxListContainer listname="결제목록" selectedBox='payList' onClick={() => handleLocation('payList')} />
+                    </ul>
                 </div>
 
             </div>
