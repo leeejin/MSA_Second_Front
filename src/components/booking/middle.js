bc43bf5a61e9860853cd62241236023ab8b60448
leeejin <asd72621@naver.com>
2024-02-22 15:33:36 +0900

    swiper 설치

--- src/components/booking/middle.js
+++ src/components/booking/middle.js
@@ -10,13 +10,51 @@ import Constant from '../../util/constant_variables';
 import AirPort from '../../util/json/airport-list';
 import Datepicker from '../../util/datepicker';
 import reverse from '../../styles/image/revert.png';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
 
+const accommodation = Constant.getSliderMenus();
 /** middle component */
 export default function MiddleComponent() {
-
+   
     return (
         <div className="container container-middle">
-            <h1>여기엔 뭘 넣지 ???</h1>
+            <div className="middlepanel">
+            <h1>실시간 인기 숙소</h1>
+            <p>다른 여행자들에게 인기 있는 숙소지를 소개합니다. 항공권을 검색하고 지금 바로 떠나세요!</p>
+            <Swiper
+                className="swiper"
+                spaceBetween={40}
+                pagination={{ clickable: true }}
+                breakpoints={{
+                  980: {
+                    slidesPerView:3.5,
+                  },
+                }}
+                  
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+            >
+                {
+                    accommodation.map((ac, i) => (
+                        <SwiperSlide 
+                        className="swiper-slide"
+                        style={{ backgroundImage: `url(${ac.imageUrl})` }} >
+                           <div className="swiper-slide-sub">
+                           <h1>{ac.value}</h1>
+                            <p>{ac.content}</p>
+                            <button className="button-reserve">
+                                예약하기
+                            </button>
+                           </div>
+                        </SwiperSlide>
+                    ))
+                }
+            </Swiper>
+            </div>
+           
         </div>
     );
 }
